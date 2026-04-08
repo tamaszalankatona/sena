70ac6655cae271d0bb5130aad82f8714dca89deb
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-04-08 12:14:26 +0200

    feat(prisma): created prisma service

---
+++ api/src/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL environment variable is not set');
+    }
+
+    const adapter = new PrismaPg({ connectionString });
+
+    super({ adapter });
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
