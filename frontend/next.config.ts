c133f5cf91b4e24ab1e1850779b0b9b614fc89d9
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2026-05-04 21:42:26 +0200

    feat: added config options for nextConfig

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  crossOrigin: "anonymous",
+  allowedDevOrigins: ["192.168.1.2"],
+  images: {
+    domains: ["images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
